a2df0de72fae00c18c3c179e0e7824e39597471b
umair2008 <84286269+umair2008@users.noreply.github.com>
2021-05-17 21:34:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('MUHAMMAD UMAIR ');
+        console.log('12 YEARS ');
+        console.log('MERITORIOUS SCHOOL ');
+        console.log('7 CLASS ');
+        console.log('PLAYING CRICKET ');
+        console.log('ZINGER BURGER ');
+        console.log('MATHS ');
+
+        alert('WELCOME 👋');
+        alert('LET ASK SOME QUESTIONS ABOUT YOU ');
+        //let name='Umair';
+       // console.log(name);
+        //let age='12';
+        //console.log(age);
+        let firstname = 'Umair';
+        let lastname = 'Shabbir';
+        
+        let space= ' ';//for creating space
+        //console.log(firstname + space + lastname + space+ 'My Age is = 12 '  );
+
+let name = prompt("What is your name ?");
+document.getElementById('myname').innerHTML=name;
+let age = prompt("What is your age ?");
+document.getElementById('myage').innerHTML=age;
+let school = prompt("in which school do you study ?");
+document.getElementById('myschool').innerHTML=school;
+let clas = prompt("in which class do you study ?");
+document.getElementById('myclass').innerHTML=clas;
+let hobbie = prompt("What is your hobbie ?");
+document.getElementById('myhobbie').innerHTML=hobbie;
+let food = prompt("What is your favourite food ?");
+document.getElementById('myfood').innerHTML=food;
+let subject = prompt("What is your favourite subject ?");
+document.getElementById('mysubject').innerHTML=subject;
+alert('THANK YOU ❤️');
